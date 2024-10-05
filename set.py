d408e36cf66acf5201b5dac2168eedaabb8d4624
rohitantonypradeep <rohitantonypradeep!@gmail.com>
2024-10-05 12:05:25 +0100

    tuples and set

---
+++ set.py
@@ -0,0 +1,9 @@
+set = {1,2,2,3,4,4,4}
+print(set)
+a = {1,2,3,4}
+d = {4,3,2,6}
+print(a.difference(d))
+print(d.difference(a))
+print(a.symmetric_difference(d))
+print(a.union(d))
+print(a.intersection(d))
---
+++ tuple.py
@@ -0,0 +1,8 @@
+a = ()
+print(a)
+b = (1,2,3)
+print(b)
+c = (1,"hello",2.3,1)
+print(c)
+print(c[2])
+print(c[0:2])
